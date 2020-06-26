1c898d663da9392607f4d2dc743681f50164b720
Seunghwan <hetzer12@icloud.com>
2020-06-26 15:36:55 +0900

    #1.4 Getting the Weather

--- App.js
+++ App.js
@@ -3,18 +3,26 @@ import React from 'react';
 import { StyleSheet, Text, View, Alert } from 'react-native';
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import axios from "axios";
+
+const API_KEY = "2e1b6dd61601283b14b84b32419b87bc";
 
 export default class extends React.Component {
   state = {
     isLoading: true
   }
 
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`);
+    console.log(data);
+  }
+
   getPosition = async () => {
     try {
       await Location.requestPermissionsAsync();
       const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
+      await this.getWeather(latitude, longitude);
       this.setState({ isLoading: false });
-      // Send to API and get Weather
     } catch (error) {
       Alert.alert("이 앱이 사용자의 위치를 찾을 수 없습니다.", "설정 > Expo > 위치 > 위치 정보 허용");
     }
