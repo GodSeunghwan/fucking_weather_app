4809ded7a1dfc94e2c63a4e9ccdf810e6fab4d53
Seunghwan <hetzer12@icloud.com>
2020-06-26 15:06:19 +0900

    #1.3 Asking for Permissions

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
 import Loading from "./Loading";
 import * as Location from "expo-location";
 
-export default class App extends React.Component {
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  }
+
   getPosition = async () => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
+    try {
+      await Location.requestPermissionsAsync();
+      const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
+      this.setState({ isLoading: false });
+      // Send to API and get Weather
+    } catch (error) {
+      Alert.alert("이 앱이 사용자의 위치를 찾을 수 없습니다.", "설정 > Expo > 위치 > 위치 정보 허용");
+    }
   }
 
   componentDidMount() {
@@ -15,8 +25,7 @@ export default class App extends React.Component {
   }
 
   render() {
-    return (
-      <Loading />
-    );
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
   }
 }
