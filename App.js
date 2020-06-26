6ffccf7490228e2f3328141b108174b4f2053c27
Seunghwan <hetzer12@icloud.com>
2020-06-26 14:41:39 +0900

    #1.2 Getting the Location

--- App.js
+++ App.js
@@ -2,9 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Loading from "./Loading";
+import * as Location from "expo-location";
 
-export default function App() {
-  return (
-    <Loading />
-  );
+export default class App extends React.Component {
+  getPosition = async () => {
+    const location = await Location.getCurrentPositionAsync();
+    console.log(location);
+  }
+
+  componentDidMount() {
+    this.getPosition();
+  }
+
+  render() {
+    return (
+      <Loading />
+    );
+  }
 }
